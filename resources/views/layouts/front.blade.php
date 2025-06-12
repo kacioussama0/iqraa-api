403f9b020d1e87cfd7c830cc0c0e30a45c268c81
KACI OUSSAMA <kacioussama0@gmail.com>
2025-06-12 04:13:20 +0100

    fix-icon-lang-size

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -128,7 +128,7 @@
 
                     <!--        Start Languages    -->
 
-                    <div class="languages d-flex">
+                    <div class="languages d-flex fs-2">
                         @if(app()->getLocale() === 'ar')
                             <a href="{{ route('change.lang', ['lang' => 'fr']) }}" class="nav-link active fw-bold fi fi-ch me-2"></a>
                         @else
